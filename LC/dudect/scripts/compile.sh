c715647bfc416a2924284750e69738cf73c782ee
Eske <eskehoy@gmail.com>
2020-05-05 21:12:15 +0200

    Fix compile script not being able to compile submissions with multiple c files

--- LC/dudect/scripts/compile.sh
+++ LC/dudect/scripts/compile.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-CFLAGS="-O2 -std=c11"
+#!/bin/bash
+CFLAGS="-O2 -std=c11 -Wfatal-errors"
 LDFLAGS="-O2 -std=c11"
 INCS="-Iinc/"
 LIBS="-lm"
@@ -11,14 +11,24 @@ for d in $SUBMISSIONS; do
     CANDIDATE=$(echo $d | cut -d'/' -f3)
     SUBMISSION=$(echo $d | cut -d'/' -f6)
     IMPLEMENTATION=$(echo $d | cut -d'/' -f7)
-    ENCRYPT_COMPILED="${d%.c}.o"
+    COMPILED=""
     INCS2="-I${d%encrypt.c}"
     DUDECT_COMPILED="compiled/${CANDIDATE}/dudect_${SUBMISSION}_${IMPLEMENTATION}"
 
     [ ! -d "compiled/$CANDIDATE" ] && mkdir -p "compiled/$CANDIDATE"
 
-    echo "Compiling $d"
-    cc $CFLAGS $INCS -c $d -o $ENCRYPT_COMPILED
+    #cc $CFLAGS $INCS -c $d -o $ENCRYPT_COMPILED
+    C_FILES=$(find -path "./candidates/${CANDIDATE}/Implementations/crypto_aead/${SUBMISSION}/${IMPLEMENTATION}/*.c")
+    for file in $C_FILES; do
+        name=$(echo $file | cut -d'/' -f8)
+        [[ "$name" = "genkat_aead.c" ]] && continue
+        echo "Compiling $file"
+        ENCRYPT_COMPILED="${file%.c}.o"
+        cc $CFLAGS $INCS -c $file -o $ENCRYPT_COMPILED
+        COMPILED="$COMPILED $ENCRYPT_COMPILED"
+    done
+
+
     echo "Compiling $DUDECT_COMPILED"
-    cc $LDFLAGS $INCS $INCS2 -o $DUDECT_COMPILED src/dut.c $OBJS $ENCRYPT_COMPILED $LIBS
+    cc $LDFLAGS $INCS $INCS2 -o $DUDECT_COMPILED src/dut.c $OBJS $COMPILED $LIBS
 done
