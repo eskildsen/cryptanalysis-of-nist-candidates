382c6be1b76680598c66f1262df86e3b47954ed9
Eske <eskehoy@gmail.com>
2020-05-08 21:15:45 +0200

    Add run script for ctgrind.

--- LC/dudect/scripts/run.sh
+++ LC/dudect/scripts/run.sh
@@ -2,16 +2,19 @@
 REF=""
 TIMEOUT=$1
 BATCH_SIZE=$3
+APPLICATION=$4
+[[ ${APPLICATION} = "ctgrind" ]] && BATCH_SIZE=1 
 [[ $2 = 1 ]] && REF="_ref"
-PROGRAMS=$(find -path "./compiled/*/dudect_*${REF}")
+PROGRAMS=$(find -path "./compiled/*/${APPLICATION}_*${REF}")
 
 OUT_DIR="out"
 [ ! -d "$OUT_DIR" ] && mkdir -p "$OUT_DIR"
-OUT_DIR="${OUT_DIR}/dudect"
+OUT_DIR="${OUT_DIR}/${APPLICATION}"
 [ ! -d "$OUT_DIR" ] && mkdir -p "$OUT_DIR"
 
-echo "Found $(echo "$PROGRAMS" | wc -l) dudect files to run"
-printf "Using $1 seconds timeout for each dudect file\n\n"
+echo "Found $(echo "$PROGRAMS" | wc -l) ${APPLICATION} files to run"
+[[ "${APPLICATION}" == "dudect" ]] && printf "Using ${TIMEOUT} seconds timeout for each dudect file"
+printf "\n\n"
 
 dudect () {
     local program=$1
@@ -29,10 +32,24 @@ dudect () {
     printf "${CANDIDATE}/${FILE} gave no output.\n"
 }
 
+ctgrind () {
+    local program=$1
+    CANDIDATE=$(echo $program | cut -d'/' -f3)
+    FILE=$(echo $program | cut -d'/' -f4)
+    [ ! -d "${OUT_DIR}/${CANDIDATE}" ] && mkdir -p "${OUT_DIR}/${CANDIDATE}"
+
+    #echo "Running ${CANDIDATE}/${FILE}"
+    valgrind $program > "${OUT_DIR}/${CANDIDATE}/${FILE}.out" 2>&1
+    OUTPUT=$(tail -n 1 ${OUT_DIR}/${CANDIDATE}/${FILE}.out)
+
+    [[ "$OUTPUT" =~ "ERROR SUMMARY" ]] && printf "${CANDIDATE}/${FILE} ${OUTPUT}\n" && return
+    printf "${CANDIDATE}/${FILE} gave no output.\n"
+}
+
 N=$BATCH_SIZE
 (
     for program in $PROGRAMS; do
         ((i=i%N)); ((i++==0)) && wait
-        dudect "$program" &
+        ${APPLICATION} "$program" &
     done
 )
